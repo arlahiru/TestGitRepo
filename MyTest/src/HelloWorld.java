5b870d5cae924107d368403f06c02a820c18a250
Lahiru Ruhunage <robinhood.lmr@gmail.com>
2012-04-07 12:26:04 +0530

    test commit

--- MyTest/src/HelloWorld.java
+++ MyTest/src/HelloWorld.java
@@ -1,12 +1,13 @@
-
 public class HelloWorld {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		System.out.println("Hello github!");
 		
-System.out.println("Hello github!");
+		System.out.println("This is a test project!");
 
 	}
 
